cfb8efc06597002bada14dfa8a15ff3cca32017d
Justin <justin@justinf.dev>
2023-01-15 05:11:11 -0800

    Implement rough map view

--- frontend/src/pages/MapScreen.js
+++ frontend/src/pages/MapScreen.js
@@ -1,68 +1,52 @@
-import { View, Button, Text, StyleSheet, Platform, Image, Dimensions } from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
+import { FloatingAction } from "react-native-floating-action";
+import MapView from 'react-native-maps';
 
-let {height, width} = Dimensions.get('window')
+const actions = [
+  {
+    text: "Add Bathroom",
+    icon: <Image source={require("../../Images/JohnLou.jpg")} style={{ width: 25, height: 25 }} />,
+    name: "bt_addbathroom",
+    position: 1,
+  },
+  {
+    text: "Options",
+    icon: require("../../Images/JohnLou.jpg"),
+    name: "bt_options",
+    position: 2,
+  },
+];
+
+const navmap = {
+  'bt_options': 'Bathroom',
+  'bt_addbathroom': 'AddBathroom'
+}
 
 export const MapScreen = ({ navigation }) => {
   return (
-    <View style={styles.MainContainer}>
-      <Text>Map Screen</Text>
-      <Button
-        title="View Bathroom"
-        onPress={() => navigation.navigate('Bathroom')}
+    <View style={styles.container}>
+      <MapView style={styles.map} />
+      <FloatingAction
+        actions={actions}
+        onPressItem={name => navigation.navigate(navmap[name])}
       />
-
-        <View style = {styles.nextBottomView}>
-
-          <Button
-          title="Add Bathroom"
-          onPress={() => navigation.navigate('AddBathroom')}
-          />
-        </View>
-
-        <View style = {styles.bottomView}>
-          <Button
-          title="Current Location(NOT WORKING)"
-          onPress={() => navigation.navigate('AddBathroom')}
-          />
-        </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create(
-  {
-    MainContainer:
-    {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-
-    image: 
-    {
-      marginTop: 50
-    },
-
-    nextBottomView:
-    {
-      width: ((height*width)/(height+width))/2,
-      height: ((height*width)/(height+width))/5,
-      backgroundColor: '#add8e6',
-      justifyContent: 'center',
-      alignItems: 'center',
-      position: 'absolute',
-      bottom: (height/12) + 8
-    },
-
-    bottomView: 
-    {
-      width: '100%',
-      height: height/12,
-      backgroundColor: '#dda0dd',
-      justifyContent: 'center',
-      alignItems: 'center',
-      position: 'absolute',
-      bottom: 0
-    },
-
-  });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+    zIndex: -1
+  },
+  icon: {
+    width: 10,
+    height: 10
+  },
+});

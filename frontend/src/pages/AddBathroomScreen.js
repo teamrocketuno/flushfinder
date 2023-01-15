5aeccef7d449ebd37d0339cd62c59e342e728646
Linda Ly <lly022@ucr.edu>
2023-01-15 02:04:50 -0800

    Add to AddBathroom

--- frontend/src/pages/AddBathroomScreen.js
+++ frontend/src/pages/AddBathroomScreen.js
@@ -1,9 +1,61 @@
-import { View, Text } from 'react-native';
+import { ImageBackground, View, Image, Text, Button, StyleSheet, TextInput  } from 'react-native';
+
+const background = '../../assets/backgrounds/bg1.png';
+const poopEmoji = '../../PoopEmoji.png';
 
 export const AddBathroomScreen = ({ navigation }) => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Add a New Bathroom</Text>
-    </View>
+    <ImageBackground source={require(background)} style={styles.backgroundImage} resizeMode='repeat'>
+      <View style={styles.appContainer}>
+        
+        <TextInput style={styles.nameInput} placeholder="Bathroom Name..." />
+        
+        <View style={styles.centerSpacingContainer}>
+          <Button
+            title="Submit"
+            onPress={() => navigation.navigate('Bathroom')}
+          />
+        </View>
+
+      </View>
+    </ImageBackground>
   );
 }
+
+const styles = StyleSheet.create({
+  backgroundImage:{
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover'
+  },
+  appContainer:{
+    backgroundColor: 'white',
+    borderRadius: 20,
+    margin: '8%',
+    opacity: 1,
+    shadowColor: "#000",
+    shadowColor: 'white',
+
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 2.8,
+    shadowRadius: 6.27,
+    
+    elevation: 10,
+  },
+  centerSpacingContainer:{
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  nameInput:{
+    borderWidth: 1,
+    borderColor: 'black',
+    width: '70%',
+    marginLeft: 50,
+    padding: 8,
+    marginBottom: 10,
+    marginTop: 10,
+  },
+});

94eee0ec180627c20407cafd409df9e3eeb2cd76
Justin <justin@justinf.dev>
2023-01-14 17:59:23 -0800

    Writing data with firestore

--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
-const { response } = require('express')
 var express = require('express')
 var fs = require('fs')
 const { request } = require('http')
 const Joi = require('joi')
+const { Firestore } = require('@google-cloud/firestore')
+const { v4: uuidv4 } = require('uuid')
+
 var app = express()
+// Authenticate using ADC and backend secret key (stored in Git secret repo)
+// see: https://cloud.google.com/docs/authentication/provide-credentials-adc#wlif-key
+const firestore = new Firestore()
 
 app.use(express.urlencoded())
 
@@ -52,50 +57,69 @@ app.post('/create', function(req, res) {
     res.end(JSON.stringify(error_response))
   } else {
     req.body = value
-    complete_response = {
-      response: 200,
-      toilet: {
-        id: 0,
-        name: req.body.name,
-        latitiude: req.body.latitude,
-        longitude: req.body.longitude,
-        likes: 0,
-        ratings: [],
-        attributes: [],
-        open: true,
-        comments: []
-      },
+
+    // Generate random id for new toilet
+    id = uuidv4()
+    created_toilet = {
+      'id': id,
+      'name': req.body.name,
+      'latitude': req.body.latitude,
+      'longitude': req.body.longitude,
+      'likes': 0,
+      'ratings': [],
+      'attributes': [],
+      'open': true,
+      'comments': []
     }
-    res.writeHead(200, {'Content-Type': 'text/json'})
-    res.end(JSON.stringify(complete_response))
+
+    // Upload to firestore collection
+    let collectionRef = firestore.collection('toilets')
+    let doc = collectionRef.doc(id)
+    doc.create(created_toilet)
+    .then(writeResult => {
+      complete_response = {
+        response: 200,
+        time: writeResult.writeTime.toDate(),
+        toilet: created_toilet
+      }
+  
+      res.writeHead(200, {'Content-Type': 'text/json'})
+      res.end(JSON.stringify(complete_response))
+    })
+    .catch((err) => {
+      console.log(err)
+      res.writeHead(500, {'Content-Type': 'text/json'})
+      error_response = { response: 500, error: 'Could not write to database.' }
+      res.end(JSON.stringify(error_response))
+    })
   }
 })
 
 // edit POST task
-app.post('/edit', function(request, response) {
+app.post('/edit', function(req, res) {
   console.log('POST /edit')
 })
 
 // rate POST task
-app.post('/rate', function(request, response) {
+app.post('/rate', function(req, res) {
   console.log('POST /rate')
 })
 
 // comment POST task
-app.post('/comment', function(request, response) {
+app.post('/comment', function(req, res) {
   console.log('POST /comment')
 })
 
 // like POST task
-app.post('/like', function(request, response) {
+app.post('/like', function(req, res) {
   console.log('POST /like')
 })
 
-app.post('/', function(request, response) {
+app.post('/', function(req, res) {
   console.log('POST /')
-  console.dir(request.body)
-  response.writeHead(200, {'Content-Type': 'text/html'})
-  response.end(request.body.name)
+  console.dir(req.body)
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  res.end(req.body.name)
 })
 
 const port = 3000
